8ccdfa7e7474ad08f3759f2d911d3b2d0fec42ed
Brendon Page <brendonpage@live.co.za>
2019-03-25 20:03:21 +0200

    Added test data builders for Entities/Value Types.

---
+++ Ls.Domain.Tests/Builders/FsDirectoryTestDataBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace Ls.Domain.Tests.Builders
+{
+    public class FsDirectoryTestDataBuilder
+    {
+        private string _name;
+
+        public static FsDirectoryTestDataBuilder Create()
+        {
+            return new FsDirectoryTestDataBuilder();
+        }
+
+        public FsDirectoryTestDataBuilder WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public FsDirectory Build()
+        {
+            return new FsDirectory
+            {
+                Name = _name
+            };
+        }
+    }
+}
---
+++ Ls.Domain.Tests/Builders/FsFileTestDataBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace Ls.Domain.Tests.Builders
+{
+    public class FsFileTestDataBuilder
+    {
+        private string _name;
+
+        public static FsFileTestDataBuilder Create()
+        {
+            return new FsFileTestDataBuilder();
+        }
+
+        public FsFileTestDataBuilder WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public FsFile Build()
+        {
+            return new FsFile
+            {
+                Name = _name
+            };
+        }
+    }
+}
