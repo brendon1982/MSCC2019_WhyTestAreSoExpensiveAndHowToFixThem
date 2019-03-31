6f14b864f2e14e4042e20987102df8b2bd721212
Brendon Page <brendonpage@live.co.za>
2019-03-31 11:00:12 +0200

    Entity/Value Type change example.

--- Ls.Domain/FsFile.cs
+++ Ls.Domain/FsFile.cs
@@ -1,7 +1,26 @@
-﻿namespace Ls.Domain
+﻿using System.Linq;
+
+namespace Ls.Domain
 {
     public class FsFile: IFsItem
     {
-        public string Name { get; set; }
+        public FsFile(string name)
+        {
+            Name = name;
+            Extension = GetExtension(name);
+        }
+
+        private string GetExtension(string name)
+        {
+            if (name.Contains('.'))
+            {
+                return name.Reverse().TakeWhile(c => c != '.').Reverse().ToString();
+            }
+
+            return "";
+        }
+
+        public string Name { get; }
+        public string Extension { get; }
     }
 }
